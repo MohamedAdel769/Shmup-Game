badbe0b2ea8a2ad77634865d73c805b1175b6cf7
MohamedAdel <mohamed.adel90211@gmail.com>
2018-11-07 12:26:05 +0200

    added player sprite and made simple movements

--- main.py
+++ main.py
@@ -8,6 +8,33 @@ pygame.display.set_caption(Title)
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedX = 0
+
+    def update(self):
+        self.speedX = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT] or keystate[pygame.K_a]:
+            self.speedX -= 8
+        if keystate[pygame.K_RIGHT] or keystate[pygame.K_d]:
+            self.speedX += 8
+        self.rect.x += self.speedX
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
 # Game loop
 running = True
 while running:
